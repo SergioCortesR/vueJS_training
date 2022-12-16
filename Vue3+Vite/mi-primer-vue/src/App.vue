1172134adbace2ead44b2d897cd1dbc83e8a8e4f
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-16 12:37:28 -0600

    Mas versiones de app en SRC

--- Vue3+Vite/mi-primer-vue/src/App.vue
+++ Vue3+Vite/mi-primer-vue/src/App.vue
@@ -1,49 +1,25 @@
 <script setup>
 
 const name = 'vue dinámico';
-const arrayFrutas = [
-  {
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-  }, {
-    name: "Platano",
-    price: "$12.00",
-    description: "Un platanoncio",
-  }, {
-    name: "Kiwi",
-    price: "$8.00",
-    description: "Uno kiwiiiiiiii",
-  }
-];
+
+let count = 0;
+const aumentarContador = () => {
+  console.log('aumentar contador');
+  count++;
+  console.log(count);
+}
+
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCase() }}</h1>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>NOMBRE</th>
-    </tr>
-    <!-- Por cada elemento del arrayFrutas genera un tablerow dentro de mi tabla -->
-    <tr v-for="(element, index) in arrayFrutas"
-    :key = element.name
-    >
-      <td>{{ index }}</td>
-      <td>{{ element.name }}</td>
-    </tr>
-  </table>
 
+  <p>
+    {{ count }}
+  </p>
+  <button v-on:click="aumentarContador()">
+    Aumentar contador
+  </button>
 </template>
 
 <style>
