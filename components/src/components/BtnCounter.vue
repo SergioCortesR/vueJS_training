6d4f0aa81b17734cd5c2b35ec91950def8698cd8
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-19 11:23:47 -0600

    Comunicacion entre componentes con emit y props con arrays

---
+++ components/src/components/BtnCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+const increment = () => counter.value++
+</script>
+
+<template>
+
+<button class="btn btn-success" v-on:click="increment()">Counter {{ counter }}</button>
+
+</template>
+<style>
+
+</style>
