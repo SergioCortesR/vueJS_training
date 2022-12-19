f755c6326fb26c7fd49a5fcb79a671d3f4db0d3f
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-19 17:30:17 -0600

    Creacion de primer ruta en vue

---
+++ vue-router1/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/pokemons',
+      name: 'pokemones',
+      component: () => import('../views/PokemonsView.vue')
+    }
+  ]
+})
+
+export default router
