790b890a692b5de5b5b22f017a1301dc6e0ab4bc
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-19 16:39:29 -0600

    Despliegue de aplicacion usando un fake api con componentes

--- components/src/App.vue
+++ components/src/App.vue
@@ -2,34 +2,61 @@
 import { ref } from 'vue';
 import BlogPost from "./components/BlogPost.vue";
 import Counter from "./components/BtnCounter.vue";
-/* Tambien es posible usar v-for en un componente pasandole los valores de un array a traves del v-bind */
-/* El ejemplo del array es más apegado a la vida real ya que se podrian consumir los elementos con una API */
-/* Nuestro array estara funcionando de forma reactvia de modod que se hara uso de ref */
-/* El array contiene un objeto por cada elemento */
-/* La emision de eventos sirve para hacer algo en un componente y este se refleje en el componente padre */
-const miArray = ref([
-    { id: 1, title: "Post 1", cont: "Contenido 1", miestilo: "success" },
-    { id: 2, title: "Post 2", cont: "Contenido 2", miestilo: "danger" },
-    { id: 3, title: "Post 3", miestilo: "primary" },
-]);
+import PaginatePost from "./components/PaginatePost.vue";
+import Spinner from "./components/LoadingSpinner.vue";
+
 
 const favorito = ref("");
-const cambiarFavorito = (titulo,segundoPa) => {
+const cambiarFavorito = (titulo, segundoPa) => {
     favorito.value = titulo;
 }
+
+const miArray = ref([])
+//Fetch sirve para consumir una api y recibirla como un json
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => miArray.value = data)
+    .catch((e) => console.log(e))
+    .finally(() => {
+        setTimeout(() => {
+            loading.value = false
+        }, 1000);
+    }
+    );
+let postXpage = 10;
+const inicio = ref(0);
+const fin = ref(postXpage);
+
+const nextPage = () => {
+    inicio.value += postXpage;
+    fin.value += postXpage;
+}
+const previusPage = () => {
+    inicio.value -= postXpage;
+    fin.value -= postXpage;
+    /* console.log("Inicio: ", inicio.value);
+    console.log("Fin: ", fin.value); */
+}
+const loading = ref(true);
 </script>
 
 <template>
-    <div class="container">
+    <Spinner v-if="loading" />
+
+    <div class="container" v-else>
         <h1>Hello world Checo!</h1>
+        <h3>Aplicacion para titulos favoritos</h3>
+
         <h2>Mi POST favorito: {{ favorito }} </h2>
-        <Counter />
-        <Counter />
-        <!-- Forma de recibir props en las etiquetas componentes -->
-        <!-- El doble punto indica que los valores se pasan a traves del v-bind -->
-        <!-- Con el @ indicamos propiedades de los componentes -->
-        <BlogPost v-for="element in miArray" :key="element.id" :id="element.id" :title="element.title"
-            :cont="element.cont" :miestilo="element.miestilo" @miAccion = "cambiarFavorito">
+        <!-- <Counter /><Counter /> -->
+
+        <PaginatePost @previusSlide="previusPage" @nextSlide="nextPage" :comienzo="inicio" :final="fin"
+            :maximo="miArray.length" class="mb-4">
+        </PaginatePost>
+        <p>Longitud del array: {{ miArray.length }}</p>
+
+        <BlogPost v-for="element in miArray.slice(inicio, fin)" :key="element.id" :id="element.id"
+            :title="element.title" :cont="element.body" @miAccionEmit="cambiarFavorito">
         </BlogPost>
 
     </div>
