7a757a82f1660bdb36e57dc7851abb7351c7d996
SergioCortesR <sergio.cortes@cceo.com.mx>
2022-12-20 18:47:55 -0600

    Composition api v-1

---
+++ composition-1/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<!-- Contador con el setup definido dentro del script -->
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const numero = ref(0);
+    const sumar = () => {
+      numero.value += 1;
+    }
+    const restar = () => {
+      numero.value -= 1;
+    }
+    return {numero, sumar, restar}
+  }
+}
+
+</script>
+<template>
+  <h1>Contador 2: {{ numero }}</h1>
+  <div class="btn btn-primary me-2" v-on:click="sumar">Sumar</div>
+  <div class="btn btn-primary" v-on:click="restar">Restar</div>
+</template>
---
+++ composition-1/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<!-- Contador con el setup como atributo del script -->
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+const numero = ref(0);
+
+const sumar = () => {
+  numero.value += 1;
+}
+const restar = () => {
+  numero.value -= 1;
+}
+
+</script>
+
+<template>
+  <h1>Contador 1: {{ numero }}</h1>
+
+  <button class="btn btn-primary me-4" v-on:click="sumar">Sumar</button>
+  <button class="btn btn-primary" v-on:click="restar">Restar</button>
+
+  <!-- <TheWelcome /> -->
+</template>
